3f969bb6199d51a3697e7c3245898a096c0ff51a
abdrakhmanovzh <abdrakhmanovzh@gmail.com>
2023-01-17 18:44:47 +0600

    forgot to change api key

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,8 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
     const { data } = await axios.get(url, {
         headers: {
-            "X-RapidAPI-Key":
-                "9b00fb23d2msh2716a8f206dd157p10d424jsnb18d84a5babc",
+            "X-RapidAPI-Key": "enter rapidapi key",
             "X-RapidAPI-Host": "bayut.p.rapidapi.com",
         },
     });
