05ff25f8690d24be5b0cbf8ab9d458763306d5be
abdrakhmanovzh <abdrakhmanovzh@gmail.com>
2023-01-17 18:48:02 +0600

    fixed again api key

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,8 @@ export const baseUrl = "https://bayut.p.rapidapi.com";
 export const fetchApi = async (url) => {
     const { data } = await axios.get(url, {
         headers: {
-            "X-RapidAPI-Key": "enter rapidapi key",
+            "X-RapidAPI-Key":
+                "9b00fb23d2msh2716a8f206dd157p10d424jsnb18d84a5babc",
             "X-RapidAPI-Host": "bayut.p.rapidapi.com",
         },
     });
